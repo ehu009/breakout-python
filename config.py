d35196a51d34c4d1109cf80fc90e8e7a4bd17c2e
ehu009 <jenssen.eirik@gmail.com>
2018-12-26 18:18:09 +0100

    loads sounds, refactored filepaths

--- config.py
+++ config.py
@@ -5,13 +5,40 @@ import pygame
 
 demo = False
 
+def full_paths (dir, names):
+    l = []
+    for name in names:
+        l.append(os.path.join(dir, name))
+    return l
+
+
 """ Images and their paths """
-image_paths = ("tileset.png","player.png", "ball.png")
-paths = []
-for p in image_paths:
-    paths.append(os.path.join("img", p))
-image_paths = paths
-#
+image_paths = full_paths("img", ("tileset.png","player.png", "ball.png"))
+
+""" Sounds and their paths """
+snd_name = ("ball.wav", "beep.wav", "pit.wav", "loss.wav", "victory.wav")
+volumes = (0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12, 0.12)
+sound_paths = full_paths("snd", snd_name)
+
+sounds = dict()
+def load_sounds():
+    c = 0
+    for p in sound_paths:
+        key = snd_name[c]
+        value = pygame.mixer.Sound(p)
+        value.set_volume(volumes[c])
+        sounds[key] = value
+        c += 1
+"""        
+    pygame.mixer.Sound(config.sound_paths[2])
+	woof_snd.set_volume(0.12)
+
+	boop_snd = pygame.mixer.Sound(config.sound_paths[1])
+	boop_snd.set_volume(0.12)
+	config.sounds.append(boop_snd)
+	config.sounds.append(woof_snd)
+"""
+
 
 """ Tiled things """
 sprite_size = 8
@@ -21,6 +48,11 @@ tileset_image = pygame.image.load(image_paths[0])
 """ Ball stuff """
 ball_size = 24
 ball_image = pygame.image.load(image_paths[2])
+
+
+""" Player restrictions """
+firing_interval = 300
+
 
 """ asdfsdaadsfa CLEAN ME UP DADDY """
 play_area_ratio = (5, 3)
