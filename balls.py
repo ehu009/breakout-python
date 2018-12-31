421e709eb22eaa5d57e4d9ca3528e86d7e8848fa
BuildTools <unconfigured@null.spigotmc.org>
2018-12-31 04:21:42 +0100

    on hitting e.g. two blocks, dont bounce twice in the same direction

--- balls.py
+++ balls.py
@@ -58,9 +58,11 @@ class Ball (pygame.sprite.Sprite, Circle):
 		
 	def bounceX(self):
 		self.vel.x *= -1
+		print "ball bounced horizontally"
 		
 	def bounceY(self):
 		self.vel.y *= -1
+		print "ball bounced vertically"
 
 	def border_collision(self, pos, arena):
 		""" Left and right boundaries of play area	"""
@@ -161,8 +163,12 @@ class Ball (pygame.sprite.Sprite, Circle):
 			self.pos.y = v.y *(hh + rad*2 +1) +(rect.y + hh)
 		if v.y > 0:
 			self.pos.y = v.y *(hh + 1) +(rect.y + hh)
-		return True
-
+		
+	@staticmethod
+	def ft(x):	
+		if x == 0:
+			return 0
+		return x/abs(x)
 		
 		
 	def update(self, time, blocks):
@@ -182,9 +188,90 @@ class Ball (pygame.sprite.Sprite, Circle):
 
 		""" interact with blocks """
 		blocks = pygame.sprite.spritecollide(self, blocks, True, intersections.intersection_BR)
-		for block in blocks:
-			self._rect_collision(block.rect)
-
+		if blocks is not None:
+			xbounce = 0
+			ybounce = 0
+			for block in blocks:
+				
+					
+				
+				# Hit-test: Rect, Ball
+				def intersection_vector (ball, rect):		
+					#	Input:	| - ball: Ball object
+					#			| - rect: Rect object
+					#		ball and rect objects have (x,y) coordinates i in top-left corner
+					#	Return:	| - If Ball og Rect har kollidert:
+					#			|  	Vector2D object indicating surface on Rect hit by Ball
+					#			| - Ellers: False
+					#
+					rad = ball.radius	#Radius
+					hw = rect.w/2		#Distance from centre of Rect to left/right edges/corners
+					hh = rect.h/2		#Distance from centre of Rect to top/bottom edges/corners
+					
+					dx = (ball.pos.x+rad)-(rect.x+hw)	#Ball x-position relative to Rect
+					dy = (ball.pos.y+rad)-(rect.y+hh)	#Ball y-position relative to Rect
+					
+					mDX = rad + hw					#Maximum dx
+					mDY = rad + hh					#Maximum dy
+					#make x, y distances into ratios
+					dx /= mDX 
+					dy /= mDY
+					
+					#make variables representing a surface on a square/rectangle
+					xdir = 0
+					ydir = 0
+					v = vector.Vector2D(0,0)
+					if abs(dx) == abs(dy):
+						if abs(dx) == 0:				#centre
+							return v
+						v.x = ft(dx)
+						v.y = ft(dy)
+						return v #corners
+					if abs(dx) > abs(dy):	#left/right sides
+						xdir = ft(dx)
+						ydir = 0
+					elif abs(dy) > abs(dx):	#top/bottom sides
+						xdir = 0
+						ydir = ft(dy)
+					v.x = xdir
+					v.y = ydir
+					return v
+				
+				v = intersection_vector(self, block.rect)
+					#reposition ball
+				hw = block.rect.w/2
+				hh = block.rect.h/2
+				rad = self.radius
+				if v.x < 0:
+					self.pos.x = v.x *(hw + rad*2 + 1) +(block.rect.x + hw)
+				if v.x > 0:
+					self.pos.x = v.x *(hw + 1) +(block.rect.x + hw)
+				if v.y < 0:
+					self.pos.y = v.y *(hh + rad*2 +1) +(block.rect.y + hh)
+				if v.y > 0:
+					self.pos.y = v.y *(hh + 1) +(block.rect.y + hh)
+				
+				
+			#	print v
+				if abs(v.x) == abs(v.y): 	#for corners
+					if ft(self.vel.y) == v.y*-1:
+						xbounce += 1
+					if ft(self.vel.x) == v.x*-1:
+						ybounce += 1
+				else:				
+					
+									#for sides
+					if v.x != 0:
+						xbounce += 1
+					if v.y != 0:
+						ybounce += 1
+				
+			if xbounce > 0:
+				self.bounceX()
+			if ybounce > 0:
+				self.bounceY()
+					
+				
 		
 
 
